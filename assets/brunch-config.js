11d8bf8b667c7c8e361644fe7fda426e1a45c42c
finnhodgkin <finnhodgkin@gmail.com>
2017-12-28 15:29:37 +0200

    :fire: removes phoenix image and adds vendor to gitignore

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -1,8 +1,10 @@
+const glob = require('glob');
+
 exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: 'js/app.js',
+      joinTo: 'js/app.js'
 
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
@@ -20,18 +22,18 @@ exports.config = {
       // }
     },
     stylesheets: {
-      joinTo: 'css/app.css',
+      joinTo: 'css/app.css'
     },
     templates: {
-      joinTo: 'js/app.js',
-    },
+      joinTo: 'js/app.js'
+    }
   },
 
   conventions: {
     // This option sets where we should place non-css and non-js assets in.
     // By default, we set this to "/assets/static". Files in this directory
     // will be copied to `paths.public`, which is "priv/static" by default.
-    assets: /^(static)/,
+    assets: /^(static)/
   },
 
   // Phoenix paths configuration
@@ -39,7 +41,7 @@ exports.config = {
     // Dependencies and current project directories to watch
     watched: ['static', 'css', 'elm', 'js', 'vendor'],
     // Where to compile files to
-    public: '../priv/static',
+    public: '../priv/static'
   },
 
   // Configure your plugins
@@ -48,17 +50,21 @@ exports.config = {
       elmFolder: 'elm',
       mainModules: ['Main.elm'],
       outputFolder: '../vendor',
-      makeParameters: ['--debug'],
+      makeParameters: ['--debug']
     },
+    swPrecache: {
+      swFileName: 'service-worker.js',
+      autorequire: [true]
+    }
   },
 
   modules: {
     autoRequire: {
-      'js/app.js': ['js/app'],
-    },
+      'js/app.js': ['js/app']
+    }
   },
 
   npm: {
-    enabled: true,
-  },
+    enabled: true
+  }
 };
